388c1ffe20f970efb757287881e5ed07190009a4
Adrien2209 <adrien.fort@free.fr>
2021-06-12 01:12:57 +0200

    Understanding of the API
    Understanding of the API :
      -  Test with system.out

---
+++ src/sample/API.java
@@ -0,0 +1,55 @@
+package sample;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpRequest;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class API {
+
+    public static JSONObject readJsonFromUrl(String url){
+        String json = "";
+
+        HttpClient client = HttpClient.newBuilder()
+                .version(Version.HTTP_1_1)
+                .followRedirects(Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(20))
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .timeout(Duration.ofMinutes(2))
+                .header("Content-Type","application/json")
+                .GET()
+                .build();
+
+        try {
+            json = client.sendAsync(request, BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body).get(10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new JSONObject(json);
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1){
+            sb.append((char) cp);
+        }
+        return sb.toString();
+
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import static sample.API.readJsonFromUrl;
 
 public class Main extends Application {
 
@@ -14,10 +24,44 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
+
+        try (Reader reader = new FileReader("data.json"))
+        {
+            /*BufferedReader rd = new BufferedReader(reader);
+            String jsonText = readAll(rd);
+            JSONObject jsonRoot = new JSONObject(jsonText);
+            JSONArray resultatRecherche = jsonRoot.getJSONObject("query").getJSONArray("search");
+            JSONObject article = resultatRecherche.getJSONObject(0);
+            System.out.println(article.getString("title"));
+            System.out.println(article.getString("snippet")); */
+            JSONObject jsonRoot = readJsonFromUrl("https://api.obis.org/v3/occurrence/grid/3?scientificname=Delphinidae");
+            JSONObject geometry = jsonRoot.getJSONArray("features").getJSONObject(0).getJSONObject("geometry");
+            JSONObject occurence = jsonRoot.getJSONArray("features").getJSONObject(0).getJSONObject("properties");
+            System.out.println(geometry.getJSONArray("coordinates"));
+            System.out.println(occurence.getInt("n"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    private static String readAll(Reader rd) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1){
+            sb.append((char) cp);
+        }
+        return sb.toString();
+
+    }
+
+
 }
